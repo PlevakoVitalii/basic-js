2ea91c73745511b8c790cb894cee85e6c7d7a491
PlevakoVitalii <plevakovl2102@gmail.com>
2021-03-26 19:03:27 +0200

    fix bug in countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+// module.exports = function countCats(matrix) {
+//   let cnt=0;
+//   const rowLength=matrix.length;
+//   for(let i=0; i<rowLength;i+=1) {
+//     const colLength=matrix[i].length;
+//     for(let j=0; j<colLength; j+=1){
+//    if (matrix[i][j]=='^^'){cnt+=1 }
+//     }
+//   }
+//   return cnt;
+// };
 
-module.exports = function countCats(arr) {
-  let counter=[];
+
+module.exports = function countCats(matrix) {
+  let counter=0;
   
-  for(let i=0; i<arr.length;i++) {
-    for(let j=0; i<arr[i].length;j++){
-   if (arr[i][j]=='^^'){counter.push(arr[i][j]) }
-
+  for(let i=0; i<matrix.length;i++) {
+    for(let j=0; j<matrix[i].length;j++){
+   if (matrix[i][j]==='^^'){counter++ }
     }
   }
-  return counter.length;
+  return counter;
 };
