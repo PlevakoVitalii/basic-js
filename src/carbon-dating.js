4458114ff62b6f2b35d03dcbe142607ecfb49c9c
PlevakoVitalii <plevakovl2102@gmail.com>
2021-03-27 08:15:07 +0200

    feat: add solution for the carbon_dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
-const CustomError = require("../extensions/custom-error");
-
+module.exports = function dateSample(sampleActivity) {
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+let age;
+let  sampleActivityNum=+sampleActivity
+  if(
+  typeof sampleActivity!=='string'
+|| sampleActivity===undefined
+  ||sampleActivityNum<0
+  ||sampleActivityNum===0
+  ||sampleActivityNum> MODERN_ACTIVITY
+  ||typeof sampleActivityNum!=='number'
+  ||isNaN(sampleActivityNum))
+  {return false}
+  const k=0.693/HALF_LIFE_PERIOD;
+  age=Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k)
+  return age
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,16 +1,3 @@
-// module.exports = function countCats(matrix) {
-//   let cnt=0;
-//   const rowLength=matrix.length;
-//   for(let i=0; i<rowLength;i+=1) {
-//     const colLength=matrix[i].length;
-//     for(let j=0; j<colLength; j+=1){
-//    if (matrix[i][j]=='^^'){cnt+=1 }
-//     }
-//   }
-//   return cnt;
-// };
-
-
 module.exports = function countCats(matrix) {
   let counter=0;
   
